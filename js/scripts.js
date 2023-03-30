56990dc030fb90e747a508443bafd2210483054c
Emma <elgerig@gmail.com>
2023-03-30 10:37:53 -0700

    test passes for validating credit card number
    Co-authored-by: Thomas McDowell <tmcdowellddhs@yahoo.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 function validate(credit) {
     const creditArray = credit.toString().split("");
+    const doubledCreditArray = []
     if (creditArray.length === 16) {
-        const doubledCreditArray = []
         for (let i = 0; i < creditArray.length; i++) {
             if (i % 2 !== 0) {
                 let sumDouble = creditArray[i] * 2
@@ -14,15 +14,14 @@ function validate(credit) {
                     doubledCreditArray.push(sum)
                 } else { doubledCreditArray.push(sumDouble) }
             } else { doubledCreditArray.push(parseInt(creditArray[i]))}
-        }
-        return doubledCreditArray
+            }
+        } else { return false; }
         let sum = 0;
-        creditArray.forEach(function(number) {
+        doubledCreditArray.forEach(function(number) {
             sum+= parseInt(number);
         })
         const sumArray = sum.toString().split("");
         if (parseInt(sumArray[sumArray.length - 1]) === 0) {
             return true;
         } else { return false; }
-    } else { return false }
 }
